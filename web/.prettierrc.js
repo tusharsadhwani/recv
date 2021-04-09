0707c13cf21f14a0a716d810ce35ab5c4a09b187
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-04-09 23:57:53 +0530

    Add styling and first page

---
+++ web/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  arrowParens: 'avoid',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  printWidth: 120,
+  proseWrap: 'always',
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+}
