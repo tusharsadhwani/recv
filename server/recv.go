f7c5a223af2c59913fd3f73d24c15e9401ebb492
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-04-10 00:50:20 +0530

    Add room code screen

--- server/recv.go
+++ server/recv.go
@@ -33,7 +33,14 @@ var upgrader = websocket.Upgrader{
 	},
 }
 
+func setupCORS(w *http.ResponseWriter) {
+	// (*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	// (*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+}
+
 func HandleConnect(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+
 	for {
 		roomCode := 10000 + rand.Intn(90000)
 		if rooms[roomCode] == nil {
