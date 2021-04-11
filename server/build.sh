cc67b29ae7e22dbe191f4ac8f8c19f90e474ed19
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-04-11 20:16:10 +0530

    Add build script

---
+++ server/build.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+rm -rf dist
+mkdir -p dist
+
+function build_platform {
+  if [[ -z $1 ]]; then
+    echo 'Error: GOOS not provided'
+    exit 1
+  fi
+  GOOS=$1
+
+  if [[ -z $2 ]]; then
+    echo 'Error: GOARCH not provided'
+    exit 1
+  fi
+  GOARCH=$2
+
+  case $GOARCH in
+    amd64)
+      PLATFORM='x86-64'
+      ;;
+    386)
+      PLATFORM='x86'
+      ;;
+  esac
+
+  FILETYPE="$GOOS-$PLATFORM"
+
+  if [[ $GOOS = windows ]]; then
+    go build -ldflags='-s' -o "dist/recv-$FILETYPE.exe" ./cmd/cli
+    go build -ldflags='-s' -o "dist/recv-server-$FILETYPE.exe" ./cmd/server
+  else
+    go build -ldflags='-s' -o 'dist/recv' ./cmd/cli
+    go build -ldflags='-s' -o 'dist/server' ./cmd/server
+    tar -czf "dist/recv-$FILETYPE.tar.gz" dist/recv
+    tar -czf "dist/recv-server-$FILETYPE.tar.gz" dist/server
+    rm dist/recv dist/server
+  fi
+  
+}
+
+build_platform "linux" "386"
+build_platform "linux" "amd64"
+build_platform "darwin" "386"
+build_platform "darwin" "amd64"
+build_platform "windows" "386"
+build_platform "windows" "amd64"
+
+echo "All files built and saved in ./dist"
