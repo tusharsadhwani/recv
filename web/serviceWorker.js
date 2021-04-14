2078a6a3599535556c7d3a6693722da8a7e931f9
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-04-14 16:32:49 +0530

    Cleanup service worker

--- web/serviceWorker.js
+++ web/serviceWorker.js
@@ -1,31 +1,26 @@
 const appName = 'recv-app'
 const assets = ['/', '/index.html', '/style.css']
-self.addEventListener('install', installEvent => {
-  installEvent.waitUntil(
-    caches.open(appName).then(cache => {
-      cache.addAll(assets)
-    }),
-  )
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(appName).then(cache => cache.addAll(assets)))
 })
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    fetch(event.request)
-      .then(response => {
-        return caches.open(appName).then(cache => {
-          cache.put(event.request, response.clone())
+    (async () => {
+      try {
+        const response = await fetch(event.request)
+        const cache = await caches.open(appName)
+        cache.put(event.request, response.clone())
+        return response
+      } catch (error) {
+        const cache = await caches.open(appName)
+        const response = await cache.match(event.request)
+        if (response != null) {
           return response
-        })
-      })
-      .catch(error => {
-        return caches.open(appName).then(cache => {
-          return cache.match(event.request).then(response => {
-            if (response != null) {
-              return response
-            }
-            throw error
-          })
-        })
-      }),
+        }
+        throw error
+      }
+    })(),
   )
 })
