4936f00312650292103b0677db569e44d1938715
Tushar Sadhwani <tushar.sadhwani000@gmail.com>
2021-07-31 22:26:23 +0530

    Add support for pre signed URL generation

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -7,7 +7,11 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
+	"time"
 
+	"github.com/google/uuid"
+	"github.com/rhnvrm/simples3"
 	"github.com/tusharsadhwani/recv"
 )
 
@@ -21,6 +25,7 @@ func RunServer() {
 	http.HandleFunc("/connect", recv.HandleConnect)
 	http.HandleFunc("/echo", recv.WebsocketEcho)
 	http.HandleFunc("/ws", recv.HandleWebsockets)
+	http.HandleFunc("/upload", GetPresignedURL)
 	http.Handle("/", http.FileServer(http.Dir("../web")))
 
 	fmt.Printf("http server started on :%d\n", port)
@@ -49,4 +54,39 @@ func getPort() int {
 	flag.IntVar(&port, "p", defaultPort, "server port")
 	flag.Parse()
 	return port
+}
+
+func GetPresignedURL(w http.ResponseWriter, req *http.Request) {
+	url, err := GeneratePresignedURL()
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+		return
+	}
+
+	w.Write([]byte(url))
+}
+
+// GeneratePresignedURL generates a pre-signed url for file upload on S3
+func GeneratePresignedURL() (string, error) {
+	cfg := GetConfig()
+
+	s3 := simples3.New(cfg.S3Region, cfg.S3AccessKey, cfg.S3SecretKey)
+
+	timestamp := time.Now()
+	uuid, err := uuid.NewRandom()
+	if err != nil {
+		return "", err
+	}
+	randomHex := strings.ReplaceAll(uuid.String(), "-", "")
+	objectKey := fmt.Sprintf("%x-%s", timestamp.Unix(), randomHex)
+
+	url := s3.GeneratePresignedURL(simples3.PresignedInput{
+		Bucket:        cfg.S3Bucket,
+		ObjectKey:     objectKey,
+		Method:        "PUT",
+		Timestamp:     timestamp,
+		ExpirySeconds: 1000,
+	})
+	return url, nil
 }
--- server/go.mod
+++ server/go.mod
@@ -2,4 +2,9 @@ module github.com/tusharsadhwani/recv
 
 go 1.16
 
-require github.com/gorilla/websocket v1.4.2
+require (
+	github.com/google/uuid v1.3.0
+	github.com/gorilla/websocket v1.4.2
+	github.com/joho/godotenv v1.3.0
+	github.com/rhnvrm/simples3 v0.7.0
+)
